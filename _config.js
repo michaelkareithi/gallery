262a0ac8a1262ab4898c3167d882a64602c9794f
MICHAEL <michaelkareithi2@gmail.com>
2025-12-03 17:10:32 +0300

    Configured Atas connection

--- _config.js
+++ _config.js
@@ -2,8 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Michael Kareithi:M0ngod13@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Michael Kareithi:M0ngod13@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Michael Kareithi:M0ngod13@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
-module.exports = config;
+module.exports = {
+  mongoURI: "mongodb+srv://Michael Kareithi:M0ngod13@cluster0.xxxxx.mongodb.net/sample_mflix?retryWrites=true&w=majority"
+};
