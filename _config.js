fea04814b18563e1233b48f88f6908766f2138d4
MICHAEL <michaelkareithi2@gmail.com>
2025-12-04 20:48:40 +0300

    merged test with master

--- _config.js
+++ _config.js
@@ -2,6 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
+<<<<<<< HEAD
     production: 'mongodb+srv://Michael Kareithi:M0ngod13@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://Michael Kareithi:M0ngod13@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://Michael Kareithi:M0ngod13@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
@@ -9,3 +10,10 @@ config.mongoURI = {
 module.exports = {
   mongoURI: "mongodb+srv://Michael Kareithi:M0ngod13@cluster0.xxxxx.mongodb.net/sample_mflix?retryWrites=true&w=majority"
 };
+=======
+    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+}
+module.exports = config;
+>>>>>>> f7a0315f241e2f713d9e8a03351e1e5c044adac0
